a4ba3769178a7164bcf8a24aa828f2227a7d6ae4
GuilhermeSalles <guibaltazarvs@gmail.com>
2024-09-30 21:01:23 -0300

    Project hr-worker created

---
+++ hr-woker/src/test/java/com/guilherme/hrworker/HrWokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
