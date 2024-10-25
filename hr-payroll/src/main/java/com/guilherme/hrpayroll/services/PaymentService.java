cb1bdd3f73b51da68126f73024ba81ee9550d0f1
GuilhermeSalles <guibaltazarvs@gmail.com>
2024-10-24 23:12:45 -0300

    Bug Arrumad era o pom que estava com dependencia do eureka sendo que nem estou usando ainda, e RestTemplete OK

--- hr-payroll/src/main/java/com/guilherme/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/guilherme/hrpayroll/services/PaymentService.java
@@ -12,18 +12,18 @@ import com.guilherme.hrpayroll.entities.Payment;
 
 @Service
 public class PaymentService {
-	
+
 	@Value("${hr-worker.host}")
 	private String workerHost;
-	
+
 	@Autowired
 	private RestTemplate restTemplate;
-	
+
 	public Payment getPayment(long workerId, int days) {
 		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", ""+workerId);
-		
-		Worker worker = restTemplate.getForObject(workerHost + "/worker/{id}", Worker.class, uriVariables);
+		uriVariables.put("id", "" + workerId);
+
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
--- hr-payroll/src/main/java/com/guilherme/hrpayroll/services/Worker.java
+++ hr-payroll/src/main/java/com/guilherme/hrpayroll/services/Worker.java
@@ -1,11 +1,8 @@
 package com.guilherme.hrpayroll.services;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 public class Worker implements Serializable {
-	
-	
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
@@ -16,6 +13,7 @@ public class Worker implements Serializable {
 	}
 
 	public Worker(Long id, String name, Double dailyIncome) {
+		super();
 		this.id = id;
 		this.name = name;
 		this.dailyIncome = dailyIncome;
@@ -25,17 +23,32 @@ public class Worker implements Serializable {
 		return id;
 	}
 
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	public String getName() {
 		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	public Double getDailyIncome() {
 		return dailyIncome;
 	}
 
+	public void setDailyIncome(Double dailyIncome) {
+		this.dailyIncome = dailyIncome;
+	}
+
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -47,8 +60,11 @@ public class Worker implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Worker other = (Worker) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-	
-	
 }
